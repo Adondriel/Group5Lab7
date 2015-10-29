906c9c71835146f3209ec882d08a86be21bcf079
Jason LoBianco <jl9857@ship.edu>
2015-10-29 18:24:22 -0400

    Updated hasweaponstate and noweaponstate to execute the actions needed.

--- src/state/HasWeaponState.java
+++ src/state/HasWeaponState.java
@@ -1,7 +1,52 @@
 package state;
 
-
+/**
+ * Provides functionality to the AI when in HasWeaponState.
+ * @author Jason LoBianco
+ */
 public class HasWeaponState extends ActionState
 {
+	AIContext ai;
 	
+	/**
+	 * Passes in the AI to be able to change the state.
+	 * @param ai
+	 */
+	public HasWeaponState(AIContext ai)
+	{
+		this.ai = ai;
+	}
+	
+	/**
+	 * When called, checks to see if the LifeForm is alive, if it is and 
+	 * there is a target for the LifeForm to attack, the LifeForm attacks 
+	 * the target.  If the LifeFrom's weapon is out of ammo, the LifeForm moves 
+	 * to OutOFAmmoState. If no target the LifeForm will turn a random direction 
+	 * and move.  If dead moves to the DeadState.
+	 */
+	public void executeAction()
+	{
+		if (alive() == true)
+		{
+			if (target() != null)
+			{
+				if (checkAmmo() == null)
+				{
+					ai.setState(ai.getOutOfAmmoState());
+				}
+				else
+				{
+					attackTarget();
+				}
+			}
+			else
+			{
+				search();
+			}
+		}
+		else
+		{
+			ai.setState(ai.getDeadState());
+		}
+	}
 }
--- src/state/NoWeaponState.java
+++ src/state/NoWeaponState.java
@@ -1,11 +1,44 @@
 package state;
 
+/**
+ * Provides functionality to the AI when in NoWeaponState.
+ * @author Jason LoBianco
+ */
 public class NoWeaponState extends ActionState
 {
-	private ActionState state;
+	AIContext ai;
 	
+	/**
+	 * Passes in the AI to be able to change the state.
+	 * @param ai
+	 */
+	public NoWeaponState(AIContext ai)
+	{
+		this.ai = ai;
+	}
+	
+	/**
+	 * When called, checks to see if the LifeForm is alive, if it is and 
+	 * there is a weapon for the LifeForm to pick up, the LifeForm picks up the weapon. 
+	 * If there is no weapon the LifeForm will turn a random direction and move.
+	 * If dead moves to the DeadState.
+	 */
 	public void executeAction()
 	{
-		
+		if (alive() == true)
+		{
+			if (weaponAtFeet() != null)
+			{
+				aquireWeapon();
+			}
+			else
+			{
+				search();
+			}
+		}
+		else
+		{
+			ai.setState(ai.getDeadState());
+		}
 	}
 }
